d5211008d6e70876714a5ddd6b2f9d932662ace6
Josh Early <josh.early@protonmail.com>
2022-12-07 08:59:59 -0500

    day06: completed part two with correct answer

--- 2022/day06/day06_p2.go
+++ 2022/day06/day06_p2.go
@@ -6,20 +6,20 @@ import (
 	"os"
 )
 
-func PartOne() {
+func PartTwo() {
 	dat, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	input_str := string(dat)
-	w := window{0, 4}
+	w := window{0, 14}
 	for w.end != len(input_str) {
 		m := make(map[rune]bool)
 		sub := input_str[w.start:w.end]
 		for _, v := range sub {
 			m[v] = true
 		}
-		if len(m) == 4 {
+		if len(m) == 14 {
 			fmt.Println(w.end)
 			break
 		}
--- 2022/day06/main.go
+++ 2022/day06/main.go
@@ -1,6 +1,16 @@
 package main
 
+type window struct {
+	start int
+	end   int
+}
+
+func (w *window) slide() {
+	w.start++
+	w.end++
+}
+
 func main() {
 	PartOne()
-	//PartTwo()
+	PartTwo()
 }
