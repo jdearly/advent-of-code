9b343fa3c4b170b4cb8f4d06ba26b5023403f9b6
Josh Early <josh.early@protonmail.com>
2022-12-07 08:56:41 -0500

    day06: completed part one with correct answer

---
+++ 2022/day06/day06_p1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type window struct {
+	start int
+	end   int
+}
+
+func (w *window) slide() {
+	w.start++
+	w.end++
+}
+
+func PartOne() {
+	dat, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	input_str := string(dat)
+	w := window{0, 4}
+	for w.end != len(input_str) {
+		m := make(map[rune]bool)
+		sub := input_str[w.start:w.end]
+		for _, v := range sub {
+			m[v] = true
+		}
+		if len(m) == 4 {
+			fmt.Println(w.end)
+			break
+		}
+		w.slide()
+	}
+}
