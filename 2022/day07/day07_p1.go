4eb444cd3c85782df1bcdd97ca8f86b46f0a31a2
Josh Early <josh.early@protonmail.com>
2022-12-08 15:23:13 -0500

    day07: complete part one with correct answer

--- 2022/day07/day07_p1.go
+++ 2022/day07/day07_p1.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func PartOne() {
-	fmt.Println("do something")
+	dat, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer dat.Close()
+
+	fs := bufio.NewScanner(dat)
+	fs.Split(bufio.ScanLines)
+
+	var total = 0
+	var stack []directory
+
+	stack = append(stack, directory{name: "/"})
+
+	for fs.Scan() {
+		line := fs.Text()
+
+		if strings.HasPrefix(line, "dir") || line == "$ cd /" || line == "$ ls" {
+			continue
+		}
+
+		fmt.Println(stack)
+		if strings.HasPrefix(line, "$ cd") {
+			if line[5:] == ".." {
+				s := stack[len(stack)-1].size
+				if s <= 100_000 {
+					total += s
+				}
+				stack = stack[:len(stack)-1]
+				stack[len(stack)-1].size += s
+			} else {
+				dir := directory{}
+				dir.name = line[5:]
+				stack = append(stack, dir)
+			}
+		} else {
+			size := strings.Split(line, " ")
+			s, _ := strconv.Atoi(size[0])
+			stack[len(stack)-1].size += s
+		}
+	}
+	fmt.Println(total)
 }
--- 2022/day07/main.go
+++ 2022/day07/main.go
@@ -1,5 +1,18 @@
 package main
 
+type directory struct {
+	size     int
+	name     string
+	parent   *directory
+	children []directory
+	files    []file
+}
+
+type file struct {
+	size int32
+	name string
+}
+
 func main() {
 	PartOne()
 	//PartTwo()
